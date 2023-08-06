73f36129e8af6f267060a88c7fb9071830cd04e3
Htin Linn Phyo <htinlinnphyo.npt@gmail.com>
2023-08-06 10:43:05 +0630

    image intervention

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\File;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
@@ -20,14 +21,22 @@ class PostController extends Controller
         ])->validate();
 
         if($request->hasFile('image')){
-            $file = $request->file('image');
-            $file_path = 'images/' . uniqid() . $file->getClientOriginalExtension();
-            Storage::disk('s3')->put($file_path, file_get_contents($file));
-            $fileModel = new File();
-            $fileModel->fie_path = $file_path;
-            $fileModel->save();
-            dd('success');
+            $orgImage = $request->file('image');
+            dd($orgImage);
+            $newImage = Image::make($orgImage)->resize(300,200);
+            dd($orgImage,$newImage);
         }
+
+
+        // if($request->hasFile('image')){
+        //     $file = $request->file('image');
+        //     $file_path = 'images/' . uniqid() . $file->getClientOriginalExtension();
+        //     Storage::disk('s3')->put($file_path, file_get_contents($file));
+        //     $fileModel = new File();
+        //     $fileModel->fie_path = $file_path;
+        //     $fileModel->save();
+        //     dd('success');
+        // }
 
     }
 
